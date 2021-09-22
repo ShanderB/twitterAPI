35894568d4acb96e611c64181e7d14f4dc58be6d
ShanderB <s.hander8910@hotmail.com>
2021-09-22 20:46:12 -0300

    getOnlyRecentTweets

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
+/*
+ * API Guide  // https://developer.twitter.com/en/docs/api-reference-index
+ *
+ * get timeline // https://developer.twitter.com/en/docs/twitter-api/v1/tweets/timelines/api-reference/get-statuses-user_timeline
+ * 
+ */
+
 var Twitter = require('twitter');
 var credenc = require('./variable.json')
-
 
 var client = new Twitter({
   consumer_key: credenc.consumer_key,
@@ -8,9 +14,14 @@ var client = new Twitter({
   bearer_token: credenc.bearer_token
 });
 
-var params = { screen_name: 'luizavienel', count: 2 };
+var params = {screen_name: 'luizavienel', count: 10, exclude_replies: true};
+
 client.get('statuses/user_timeline', params, function (error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    console.log(Object.keys(tweets));
+
+    tweets.forEach(element => {
+      console.log(`"${element.text}"`);
+    });
   }
 });
