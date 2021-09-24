b1847971b910964e62efec5462d8595edf901d9b
ShanderB <s.hander8910@hotmail.com>
2021-09-23 21:41:33 -0300

    postAdd

--- test.js
+++ test.js
@@ -11,9 +11,11 @@ var credenc = require('./variable.json')
 var client = new Twitter({
   consumer_key: credenc.consumer_key,
   consumer_secret: credenc.consumer_secret,
-  bearer_token: credenc.bearer_token
+  access_token_key: credenc.access_token,
+  access_token_secret: credenc.access_token_secret,
 });
 
+/* 
 var params = {screen_name: 'luizavienel', count: 10, exclude_replies: true};
 
 client.get('statuses/user_timeline', params, function (error, tweets, response) {
@@ -24,4 +26,27 @@ client.get('statuses/user_timeline', params, function (error, tweets, response) 
       console.log(`"${element.text}"`);
     });
   }
-});
+}); */
+
+/* 
+client.post('statuses/update', {status: 'I L2ov3e Twitter'})
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(`\n=\n==\n====\n=================${error[0].message}=================\n====\n===\n==\n=`)
+  })
+ */
+
+
+
+  var id = 1441182358049202188
+
+client.post('statuses/destroy/1441182358049202188', {id: id}).then((response) => {  //! Verificar como enviar a requisição. Chumbando o valor, passa normal
+  console.log(response)
+}).catch((error) => {
+  console.log(error)
+  // console.log(`\n=\n==\n====\n=================${error[0].message}=================\n====\n===\n==\n=`)
+})
+
+
