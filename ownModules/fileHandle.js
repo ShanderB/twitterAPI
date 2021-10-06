5fbab501267480152bda5563bba1f65b297ff9a5
ShanderB <s.hander8910@hotmail.com>
2021-10-05 21:53:20 -0300

    created

--- ownModules/fileHandle.js
+++ ownModules/fileHandle.js
@@ -1,21 +1,21 @@
 const fs = require('fs');
 
 async function readFile() {
-    return new Promise((resolve, reject) => {
-      fs.readFile('./data/config.txt', function (err, data) {
-        if (err) reject(err);
-        resolve(data);
-      });
+  return new Promise((resolve, reject) => {
+    fs.readFile('./data/config.txt', function (err, data) {
+      if (err) reject(err);
+      resolve(data);
     });
-  }
+  });
+}
 
-  async function writeFile() {
-    return new Promise((resolve, reject) => {
-      fs.writeFile('./data/config.txt', function (err, data) {
-        if (err) reject(err);
-        resolve(data);
-      });
+async function writeFile(dados) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./data/config.txt', dados, function (err) {
+      if (err) reject(err);
+      resolve(dados);
     });
-  }
+  });
+}
 
-module.exports = readFile;
+module.exports = { readFile, writeFile };
